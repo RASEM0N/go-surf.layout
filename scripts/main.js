5a59f1f3da00542d6605011e23818b5776b47f23
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-09-01 16:30:52 +0500

    init

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+function windowWidth() {
+    const element = document.createElement('div')
+    element.innerText = `${innerWidth}px/${innerHeight}px`
+    element.style.position = 'fixed'
+    element.style.backgroundColor = '#030303'
+    element.style.padding = '10px'
+    element.style.top = '20px'
+    element.style.right = '20px'
+    element.style.zIndex = '11'
+    element.style.fontFamily = 'SF Display Pro'
+    element.style.fontWeight = 'bold'
+    element.style.fontSize = '24px'
+    element.style.color = '#ffffff'
+
+    document.body.append(element)
+
+    window.addEventListener('resize', () => {
+        element.innerText = `${innerWidth}px/${innerHeight}px`
+    })
+}
+
+// windowWidth()
+
